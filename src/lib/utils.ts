5c53a81478ef0d1daded9890d2560260dc4d9980
Tanmay <tanmayprajapat589@gmail.com>
2026-06-07 17:02:56 +0530

    useMediaSources complete...

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,42 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import axios from "axios"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+const httpClient = axios.create({
+  baseURL:import.meta.env.VITE_HOST_URL
+})
+
+export const onCloseApp = () =>  window.ipcRenderer.send('closeApp')
+
+
+
+
+
+export const fetchUserProfile = async(clerkId:string)=>{
+
+  const response = await httpClient.get(`/auth/${clerkId}`,{
+    headers:{
+      "Content-Type":"application/json",
+    },
+  })
+  return response.data
+
+}
+
+
+
+export const getMediaSources = async () => {
+  const displays = await window.ipcRenderer.invoke('getSources')
+  const enumerateDevices =
+    await window.navigator.mediaDevices.enumerateDevices()
+  const audioIputs = enumerateDevices.filter(
+    (device) => device.kind === 'audioinput'
+  )
+
+  console.log('getting sources')
+  return { displays, audio: audioIputs }
 }
